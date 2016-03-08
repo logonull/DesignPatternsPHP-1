e0352639ca4ac6c50d5e0b5b28bd69ef84598d4c
ystop <ystop.top@gmail.com>
2016-03-08 19:57:06 +0800

    装饰器模式

--- Structural/Decorator/Decorator.php
+++ Structural/Decorator/Decorator.php
@@ -3,13 +3,11 @@
 namespace DesignPatterns\Structural\Decorator;
 
 /**
- * the Decorator MUST implement the RendererInterface contract, this is the key-feature
- * of this design pattern. If not, this is no longer a Decorator but just a dumb
- * wrapper.
+ * 装饰器必须实现RendererInterface规约，这是这个设计模式的核心特性.如果没有，这就不是一个装饰器而仅仅是一个沉默的包装者
  */
 
 /**
- * class Decorator.
+ *  Decorator类
  */
 abstract class Decorator implements RendererInterface
 {
@@ -19,9 +17,8 @@ abstract class Decorator implements RendererInterface
     protected $wrapped;
 
     /**
-     * You must type-hint the wrapped component :
-     * It ensures you can call renderData() in the subclasses !
-     *
+     * 你必须类型提示包裹的组件
+     * 它确认你能调用renderData()方法在你的子类里.
      * @param RendererInterface $wrappable
      */
     public function __construct(RendererInterface $wrappable)
